de35744df73b4ef3242b8a21030edb336d766dd6
Robin van den Bogaard <rjd.vandenbogaard@gmail.com>
2022-12-26 10:52:47 +0100

    Initial commit

---
+++ src/test/java/com/robinthedev/outbox/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package com.robinthedev.outbox;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
